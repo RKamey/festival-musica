59e89a7c90722de56eea68c97fb76b0256b0bd94
RKamey <ralonso@ucol.mx>
2024-01-09 20:41:24 -0600

    feat(gulp): actualizar tarea para compilar todos los estilos SCSS en todas las carpetas

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
-function css(done) {
-    src('src/scss/app.scss') // Identificar el archivo SASS
+function css( done ) {
+    src('src/scss/**/*.scss') // Identificar el archivo SASS
     .pipe( sass() ) // Compilarlo
     .pipe(dest("build/css")); // Almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
-function dev(done) {
-    watch("src/scss/app.scss", css);
+function dev( done ) {
+    watch("src/scss/**/*.scss", css);
 
     done();
 }
