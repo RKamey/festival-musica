3c1a354eee59067545383331637a7548d797a878
RKamey <ralonso@ucol.mx>
2024-01-09 20:53:09 -0600

    feat: instalar Plumber y agregar función al gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
-const { src, dest, watch } = require("gulp");
-const sass = require("gulp-sass")(require('sass'));
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( done ) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
+    .pipe( plumber())
     .pipe( sass() ) // Compilarlo
-    .pipe(dest("build/css")); // Almacenarla en el disco duro
+    .pipe(dest('build/css')); // Almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
 function dev( done ) {
-    watch("src/scss/**/*.scss", css);
+    watch('src/scss/**/*.scss', css);
 
     done();
 }
