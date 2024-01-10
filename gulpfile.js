30d36c076c3a7e4f7559f6ac58082827de3b5c4c
RKamey <ralonso@ucol.mx>
2024-01-09 19:41:36 -0600

    build(gulp): configurar tarea para compilar y guardar estilos Sass

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest } = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
+
+function css(done) {
+    src('src/scss/app.scss') // Identificar el archivo SASS
+    .pipe( sass() ) // Compilarlo
+    .pipe(dest("build/css")); // Almacenarla en el disco duro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+exports.css = css;
