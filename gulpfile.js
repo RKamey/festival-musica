582dc2e995509964127db7aac8d75b0365085bb3
RKamey <ralonso@ucol.mx>
2024-01-13 16:40:23 -0600

    feat: cambiar formatos a WebP, optimizar imágenes y agregar estilos de festival (_festival.scss)

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,47 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
 function css( done ) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
-    .pipe( plumber())
-    .pipe( sass() ) // Compilarlo
-    .pipe(dest('build/css')); // Almacenarla en el disco duro
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe(dest('build/css')); // Almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function imagenes( done ) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img'))
+
+    done();
+}
+
+function versionWebp( done ) {
+    
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+    
+    done();
 }
 
 function dev( done ) {
@@ -18,4 +51,6 @@ function dev( done ) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel( imagenes, versionWebp, dev );
