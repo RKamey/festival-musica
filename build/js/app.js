fd0111e58a9a05581d584da4b1e3c1b525d75651
RKamey <ralonso@ucol.mx>
2024-01-13 21:40:58 -0600

    feat: fijar barra al dar scroll

--- build/js/app.js
+++ build/js/app.js
@@ -1,10 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     iniciarApp();
-    scrollNav();
 });
 
 function iniciarApp() {
+    navegacionFija();
     crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function () {
+        
+        if( sobreFestival.getBoundingClientRect().bottom < 0 ) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    })
 }
 
 function scrollNav() {
