d478c19c3444e6f30fd9bd3d01150d8aa7c0891e
RKamey <ralonso@ucol.mx>
2024-01-13 21:51:09 -0600

    chore: instalar y agregar sourcemaps a la tarea CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -15,9 +16,11 @@ const avif = require('gulp-avif');
 
 function css( done ) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
+        .pipe(sourcemaps.init())
         .pipe( plumber())
         .pipe( sass() ) // Compilarlo
         .pipe( postcss([autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css')); // Almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
