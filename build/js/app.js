8076af705458817e2e763ca8584175fe0f309e55
RKamey <ralonso@ucol.mx>
2024-01-13 21:21:51 -0600

    feat: renombrar archivo galeria.js por app.js, agregar efecto smooth al scroll

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     iniciarApp();
+    scrollNav();
 });
 
 function iniciarApp() {
     crearGaleria();
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
+    });
 }
 
 function crearGaleria() {
